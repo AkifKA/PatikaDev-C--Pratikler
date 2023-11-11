95adf8bfc48d80cf35d6d3f255a2334fa0d9ce5d
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-11-11 15:19:17 +0300

    Array.Sort

--- diziler-array-sinifi-metotlari/Program.cs
+++ diziler-array-sinifi-metotlari/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Sort
+        int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 11, 17 };
+        Console.WriteLine("***** Sırasız Sayı Dizisi");
+        foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+        Console.WriteLine("***** Sıralı Sayı Dizisi");
+        Array.Sort(sayiDizisi);
+
+
     }
 }
