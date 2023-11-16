9e4e0a16577411b91270ae6c6aef2f9aead3976b
MAK <karaozmehmetakif@gmail.com>
2023-11-16 23:13:03 +0300

    Rekursif ornek

--- recursive-extension-metotlar/Program.cs
+++ recursive-extension-metotlar/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //? Rekursif - Öz yinemeli Fonksiyomlar/Metotlar
+
+        //? Örnek 3^4 işlemi. 3^4=3*3*3*3
+        int result = 1;
+        for (int i = 1; i < 5; i++)
+        {
+            result *= 3;
+        }
+        Console.WriteLine(result);
+        Islemler instance = new Islemler();
+        Console.WriteLine(instance.Expo(3,4));
+
+    }
+
+    public class Islemler
+    {
+        public int Expo(int sayi, int us)
+        {
+            if(us<2)
+                return sayi;
+            return Expo(sayi, us-1)*sayi;
+        }
+        //? Expo(3,4)
+        //? Expo(3,3) * 3
+        //? Expo(3,2) * 3 * 3
+        //? Expo(3,1) * 3 * 3 * 3
+        //?     3 * 3 * 3 * 3     = 3^4
     }
 }
