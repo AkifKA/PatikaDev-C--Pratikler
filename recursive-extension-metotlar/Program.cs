b64be2be9ca4cf4930f515147d15e8807ef78166
MAK <karaozmehmetakif@gmail.com>
2023-11-17 00:07:59 +0300

    finished

--- recursive-extension-metotlar/Program.cs
+++ recursive-extension-metotlar/Program.cs
@@ -5,7 +5,6 @@ class Program
     static void Main(string[] args)
     {
         //? Rekursif - Öz yinemeli Fonksiyomlar/Metotlar
-
         //? Örnek 3^4 işlemi. 3^4=3*3*3*3
         int result = 1;
         for (int i = 1; i < 5; i++)
@@ -15,6 +14,31 @@ class Program
         Console.WriteLine(result);
         Islemler instance = new Islemler();
         Console.WriteLine(instance.Expo(3,4));
+
+        //? Extension Metotlar
+        string ifade = "Akif Karaöz";
+        bool sonuc = ifade.CheckSpaces();
+        Console.WriteLine(sonuc);
+        if (sonuc)
+        {
+            Console.WriteLine(ifade.RemoveWhiteSpaces());
+        }
+        Console.WriteLine(ifade.MakeUpperCase());
+        Console.WriteLine(ifade.MakeLowerCase());
+
+        int[] dizi = {9,5,3,1,7,4};
+        dizi.SortArray();
+        dizi.EkranaYazdir();
+
+        int sayi = 50;
+       sayi.IsEven();
+
+       string ifade2 = "Patika";
+       Console.WriteLine(ifade2.GetFirstCharacter());
+
+
+        
+
 
     }
 
@@ -32,4 +56,54 @@ class Program
         //? Expo(3,1) * 3 * 3 * 3
         //?     3 * 3 * 3 * 3     = 3^4
     }
-}
+    }
+
+   public static class Extension{
+        public static bool CheckSpaces(this string param){
+            return param.Contains(" ");
+        }
+
+        public static string RemoveWhiteSpaces(this string param){
+                string[] dizi = param.Split(" ");
+                return string.Join("", dizi);
+        }
+
+        public static string MakeUpperCase(this string param){
+            return param.ToUpper();
+        }
+        public static string MakeLowerCase(this string param){
+            return param.ToLower();
+        }
+    
+    public static int[] SortArray(this int[] param){
+        Array.Sort(param);
+        return param;
+    }
+    
+    public static void EkranaYazdir(this int[] param){
+        foreach (int item in param)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    public static bool IsEven(this int param)
+    {
+        bool sonuc = param % 2 == 0;
+        if (sonuc)
+            Console.WriteLine("{0} çifttir", param);
+        else
+            Console.WriteLine("{0} tektir", param);
+        return sonuc;
+    }
+
+    public static string GetFirstCharacter(this string param)
+    {
+            return param.Substring(0,1);
+    }
+    
+    
+    }
+ 
+ 
+
+
